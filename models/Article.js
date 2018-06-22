3c86ddc8f8f99165d2a7204764b9d79cab49a461
Chris Stavnes <32145337+CStavnes1@users.noreply.github.com>
2018-06-21 20:33:03 -0500

    Update Article.js

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+  // `link` is required and of type String
+  link: {
+    type: String,
+    required: true
+  },
+
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+
+module.exports = Article;
